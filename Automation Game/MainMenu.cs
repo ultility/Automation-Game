c64889983de9562fb923ef89ccf9404ae943f0de
ultility <bkjnbkjn@gmail.com>
2021-10-08 14:16:20 +0300

    added a cool mainmenu background

--- Automation Game/MainMenu.cs
+++ Automation Game/MainMenu.cs
@@ -2,7 +2,12 @@
 using Android.Content;
 using Android.OS;
 using Android.Widget;
+using Android.Graphics;
 using System;
+using Automation_Game.Map;
+using System.Timers;
+using Android.Graphics.Drawables;
+using System.IO;
 
 namespace Automation_Game
 {
@@ -11,6 +16,15 @@ namespace Automation_Game
     {
         Button load;
         Button start;
+        Bitmap FullBackground;
+        Bitmap background;
+        LinearLayout backdrop;
+        Rect src;
+        Rect dst;
+        Timer t;
+        double dx;
+        double left;
+        double right;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -20,6 +34,40 @@ namespace Automation_Game
             start = (Button)FindViewById(Resource.Id.start);
             load.Click += Load_Click;
             start.Click += Start_Click;
+            int width = Window.WindowManager.CurrentWindowMetrics.Bounds.Width();
+            int height = Window.WindowManager.CurrentWindowMetrics.Bounds.Height();
+            background = Bitmap.CreateBitmap(width, height, Bitmap.Config.Argb8888);
+            backdrop = (LinearLayout)FindViewById(Resource.Id.backdrop);
+            Canvas draw = new Canvas(background);
+            FullBackground = BitmapFactory.DecodeResource(Resources, Resources.GetIdentifier("mainmenu_background", "drawable", PackageName));
+            src = new Rect(0, 0, draw.Width, draw.Height);
+            dst = new Rect(0, 0, draw.Width, draw.Height);
+            left = src.Left;
+            right = src.Right;
+            draw.DrawBitmap(FullBackground, src, dst, null);
+            t = new Timer(1000 / 30.0);
+            t.Elapsed += T_Elapsed;
+            t.Enabled = true;
+            dx = Math.Abs(FullBackground.Width - background.Width);
+            dx = dx / (60 * 30);
+           backdrop.Background = new BitmapDrawable(Resources, background);
+        }
+
+        private void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            t.Enabled = false;
+            left += dx;
+            right += dx;
+            src.Left = (int)left;
+            src.Right = (int)right;
+            if (src.Right >= FullBackground.Width || src.Left <= 0)
+            {
+                dx = -dx;
+            }
+            Canvas draw = new Canvas(background);
+            draw.DrawBitmap(FullBackground, src, dst, null);
+            backdrop.Background = new BitmapDrawable(Resources, background);
+            t.Enabled = true;
         }
 
         private void Start_Click(object sender, EventArgs e)
