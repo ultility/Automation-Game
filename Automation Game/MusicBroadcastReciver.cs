d47ba0bcdcc2803fab87ac2b3edc0e7ec5d85cd8
schoolrupin00 <schoolrupin00@gmail.com>
2021-12-07 11:27:35 +0200

    added music service

---
+++ Automation Game/MusicBroadcastReciver.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Media;
+
+namespace Automation_Game
+{
+    [BroadcastReceiver]
+    public class MusicBroadcastReciver : BroadcastReceiver
+    {
+        MediaPlayer menu, game;
+        public MusicBroadcastReciver(MediaPlayer menu, MediaPlayer game)
+        {
+            this.menu = menu;
+            this.game = game;
+        }
+        public override void OnReceive(Context context, Intent intent)
+        {
+            int play = intent.GetIntExtra("music", -1);
+            switch (play)
+            {
+                case 0:
+                    game.Stop();
+                    menu.Start();
+                    break;
+                case 1:
+                    menu.Stop();
+                    game.Start();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Automation Game/MusicService.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Automation_Game
+{
+    [Service]
+    class MusicService : Service
+    {
+        MusicBroadcastReciver broadcastReciver;
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            MediaPlayer menu = MediaPlayer.Create(this, Resource.Raw.Minecraft_Music___Menu_1);
+            MediaPlayer game = MediaPlayer.Create(this, Resource.Raw.Terraria_Soundtrack___01___Overworld_Day);
+            broadcastReciver = new MusicBroadcastReciver(menu, game);
+            IntentFilter f = new IntentFilter();
+            RegisterReceiver(broadcastReciver, f);
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            return base.OnStartCommand(intent, flags, startId);
+        }
+    }
+}
